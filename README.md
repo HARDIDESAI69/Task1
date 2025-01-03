6b6283bc82de4f71b7c48635cd10f6a26b043d59
HARDIDESAI69 <hardidesai799@gmail.com>
2025-01-03 11:25:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+![Screenshot (388)](https://github.com/Shreyanshi200/futureofgaming/assets/120807907/81ba8f62-0e26-4c3a-b936-b2834f0e5588)
+
+  2) In the terminal, run `npm install`
+  3) Run `npm start` to view project in browser
+  
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const port = 5001;
+const path = require("path");
+
+// CORS middleware to allow requests from your React app
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static("build"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+// Sample data
+const settingsData = {
+  region: "europe - de",
+  punkbuster: "on",
+  fairfight: "on",
+  password: "off",
+  preset: "normal",
+};
+
+const advancedData = {
+  minimap: "on",
+  onlySquadLeaderSpawn: "off",
+  vehicles: "on",
+  teamBalance: "on",
+  minimapSpotting: "on",
+  hud:"on",
+  Pvehiclecam:"on",
+  regenerativehealth:"on",
+  killcam:"on",
+  friendlyfire:"off",
+  dspotting:"on",
+  enemynametags:"on",
+};
+
+const rulesData = {
+  tickets: 400,
+  vehicleSpawnDelay: 25,
+  bulletDamage: 100,
+  kickAfterTeamKills: 5,
+  playerHealth: 100,
+  playerrespawn:100,
+  kickafteridle:300,
+  banafterkicks:3,
+
+};
+
+// Endpoints
+app.get('/api/settings', (req, res) => {
+  res.json(settingsData);
+});
+
+app.get('/api/advanced', (req, res) => {
+  res.json(advancedData);
+});
+
+app.get('/api/rules', (req, res) => {
+  res.json(rulesData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
